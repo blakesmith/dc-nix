d2c7517bcb0f3603ecf61390907416c9457d6daf
Blake Smith <blakesmith0@gmail.com>
2024-11-27 19:22:52 -0600

    Add DNS module

--- modules/terraform/default.nix
+++ modules/terraform/default.nix
@@ -3,5 +3,6 @@
 {
   imports = [
     ./hosts.nix
+    ./dns.nix
   ];
 }
---
+++ modules/terraform/dns.nix
@@ -0,0 +1,52 @@
+{ config, lib, ... }:
+
+with builtins;
+
+let
+  publicZone = config.dc.region.dnsZones.public.name;
+
+  mapAttrsFilter = m: pred: attrs:
+    (listToAttrs (map m (filter pred (lib.attrsets.attrsToList attrs))));
+
+  hostRecords = {
+    resource.aws_route53_record = (mapAttrsFilter
+      (hostAndValue: {
+        name = hostAndValue.name;
+        value = {
+          zone_id = "\${aws_route53_zone.public-zone.zone_id}";
+          name = "${hostAndValue.name}.${publicZone}";
+          type = "A";
+          ttl = "300";
+          records = [
+            hostAndValue.value.internalIpv4Address
+          ];
+        };
+      })
+      (hostAndValue: (builtins.hasAttr "internalIpv4Address" hostAndValue.value))
+      config.dc.region.hosts);
+  };
+
+  endpointRecords = {
+    resource.aws_route53_record = builtins.mapAttrs (name: endpoint: {
+      zone_id = "\${aws_route53_zone.public-zone.zone_id}";
+      name = endpoint.address;
+      type = "A";
+      ttl = "300";
+      records = endpoint.dns.records;
+    }) config.dc.region.httpEndpoints;
+  };
+
+  cfg = config.dc.terraform.dns;
+in
+
+{
+  options.dc.terraform.dns = {
+    enable = lib.mkEnableOption "Enable DNS provisioning";
+  };
+
+  config = lib.mkIf cfg.enable (lib.attrsets.recursiveUpdate (lib.attrsets.recursiveUpdate hostRecords endpointRecords) {
+    resource.aws_route53_zone."public-zone" = {
+      name = config.dc.region.dnsZones.public.name;
+    };
+  });
+}
